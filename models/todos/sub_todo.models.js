0c315aa2236aceaff3eda92a5760531900e67c9b
Harsh Guleria <guleriah59@gmail.com>
2025-10-28 11:08:28 +0530

    add backend code

--- models/todos/sub_todo.models.js
+++ models/todos/sub_todo.models.js
@@ -1,5 +1,21 @@
 import mongoose from "mongoose";
 
-const subTodoSchema = new mongoose.Schema({}, { timestamps: true });
+const subTodoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      requires: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 export const SubTodo = mongoose.model("SubTodo", subTodoSchema);
--- models/todos/todo.models.js
+++ models/todos/todo.models.js
@@ -1,5 +1,28 @@
 import mongoose from "mongoose";
 
-const todoSchema = new mongoose.Schema({}, { timestamps: true });
+const todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete:{
+        type: Boolean,
+        default: false,
+    },
+    createdBy: {
+        tyoe: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    subTodos: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "SubTodo",
+
+        }
+    ] // Array of Sub-Todos
+  },
+  { timestamps: true }
+);
 
 export const Todo = mongoose.model("Todo", todoSchema);
